8d3ef227cbe8ab7f74b28ad1e24a8828b5012285
Kawika Kekahuna <kawikadkekahuna@github.com>
2015-05-27 10:07:54 -1000

    4/4 challenges completed

--- multiples.js
+++ multiples.js
@@ -7,6 +7,11 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
-
-  return sum;
-};
+	for (var i = n - 1; i>= 0; i--){
+		if(i%3 === 0 || i % 5 === 0){
+			sum += i;
+		}
+		
+	};
+	return sum;
+}
